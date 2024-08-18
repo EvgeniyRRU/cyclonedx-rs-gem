e726a84496d6ef7b77076c00390a3a4f47871312
Evgeniy Chernyshev <evgeniyrru@gmail.com>
2024-08-18 21:05:31 +0200

    Fix error messages

--- src/errors.rs
+++ src/errors.rs
@@ -5,18 +5,18 @@ use thiserror::Error;
 /// 
 #[derive(Error, Debug)]
 pub(crate) enum FetchPackageError {
-    #[error("Could not send request for `{0}` for gem `{1}`")]
+    #[error("Could not send request for gem {0} version {1}")]
     SendRequestError(String, String),
-    #[error("Could not receive response text for `{0}` for gem `{1}`")]
+    #[error("Could not receive response text for {0} for gem {1}")]
     ResponseTextError(String, String),
-    #[error("Could not find version `{0}` for gem `{1}`")]
+    #[error("Could not find version {0} for gem {1}")]
     VersionNotFound(String, String),
-    #[error("Gem not found: `{0}`, version `{1}`")]
+    #[error("Gem not found: {0}, version {1}")]
     PackageNotFound(String, String),
-    #[error("Client error occurred for gem `{0}`, version `{1}`")]
+    #[error("Client error occurred for gem {0}, version {1}")]
     ClientError(String, String),
-    #[error("Server error occurred for gem `{0}`, version `{1}`")]
+    #[error("Server error occurred for gem {0}, version {1}")]
     ServerError(String, String),
-    #[error("Unknown error occurred for gem `{0}`, version `{1}`")]
+    #[error("Unknown error occurred for gem {0}, version {1}")]
     UnknownError(String, String),
 }
